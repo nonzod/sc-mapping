0b221292db9ee234d96629f02f00fb1ca77a74f8
Nicola Tomassoni <n.tomassoni@tourtools.it>
2024-09-20 19:04:08 +0200

    Rimossi test interact

--- pages/bindings/[uuid].vue
+++ pages/bindings/[uuid].vue
@@ -4,13 +4,6 @@
     Loading ...
   </div>
   <div class="" v-else>
-    <div class="container">
-      {{ position }}
-      <div ref="interactableTarget" style="width: 100px; height: 100px; background-color: #29e;"></div>
-      <div ref="interactableTarget2" style="width: 50px; height: 50px; background-color: #ddd;"></div>
-      <button class="reset-btn" @click="reset">Reset</button>
-    </div>
-
     <div class="flex">
       <component :is="DeviceType" :items="items" :device="device" class="w-1/2" v-for="device in devices" />
     </div>
@@ -18,9 +11,6 @@
   </div>
 </template>
 <script setup lang="ts">
-import { useInteractContext, useDraggable } from 'vue-interact';
-import interact from "interactjs";
-
 const route = useRoute()
 const { gtag } = useGtag()
 
@@ -35,40 +25,6 @@ gtag('event', 'screen_view', {
 })
 
 const DeviceType = defineAsyncComponent(() =>
-  import(`../../components/Devices/${profile.value.device_type}.vue`)
+  import(`../../components/Devices/${profile?.value?.device_type}.vue`)
 )
-
-// Test interact
-const interactableTarget = ref(null);
-const interactableTarget2 = ref(null);
-
-const context = useInteractContext(interactableTarget);
-const { init, position } = useDraggable(context);
-
-const context2 = useInteractContext(interactableTarget2);
-const { init: init2, position: position2 } = await useDraggable(context2, {
-  modifiers: [
-    interact.modifiers.snap({
-      targets: [
-        interact.snappers.grid({ x: 30, y: 30 })
-      ],
-      range: Infinity,
-      relativePoints: [{ x: 0, y: 0 }]
-    })
-  ],
-  inertia: true
-})
-
-onMounted(() => {
-  nextTick(() => {
-    init()
-    init2()
-    reset()
-  })
-})
-
-const reset = () => {
-  position2.value.x = 100
-  position2.value.y = 300
-}
 </script>
