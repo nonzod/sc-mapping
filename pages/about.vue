615ca8246538dfaca4654e69dd9b1d9c0e2de472
Nicola Tomassoni <n.tomassoni@tourtools.it>
2024-09-23 09:52:27 +0200

    Fankit

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,7 @@
       Questo tool esiste perché volevo studiarmi NuxtJs e perché mi piace Star Citizen.
     </p>
     <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-      ...
+      Star Citizen®, Roberts Space Industries® and Cloud Imperium ® are registered trademarks of Cloud Imperium Rights LLC
     </p>
     <canvas id="canvas" width="300" height="300"></canvas>
   </div>
