8421f63dd834be002649f6ae7774f423553f9d34
Nicola Tomassoni <n.tomassoni@tourtools.it>
2024-10-02 14:45:50 +0200

    aggiunto username al profilo

--- server/api/profiles.post.ts
+++ server/api/profiles.post.ts
@@ -1,7 +1,7 @@
-import { profile as ProfileTable } from '~/db/schema'
+import { profile as ProfileTable, user as UserTable } from '~/db/schema'
+import { eq } from "drizzle-orm";
 
 export default defineEventHandler(async (event) => {
-  //const { user } = await requireUserSession(event)
   const { p: page, ps: pageSize } = await readBody<{ p: number, ps: number }>(event)
 
   try {
@@ -11,9 +11,12 @@ export default defineEventHandler(async (event) => {
         name: ProfileTable.name,
         version: ProfileTable.version,
         filepath: ProfileTable.filepath,
-        device_type: ProfileTable.device_type
+        device_type: ProfileTable.device_type,
+        username: UserTable.username,
+        user_id: UserTable.id
       })
       .from(ProfileTable)
+      .leftJoin(UserTable, eq(ProfileTable.user_id, UserTable.id))
       .limit(pageSize)
       .offset((page - 1) * pageSize)
     return res_profiles;
